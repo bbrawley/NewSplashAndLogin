99ba4d9d3024c6dc025669e88b45ebeb92f88fe9
bbrawley <brandon@iempireapps.com>
2018-02-12 11:08:25 -0500

    Attempted to add code to App.cs. Still have exception.

--- NewSplashAndLogin/NewSplashAndLogin.Android/App.cs
+++ NewSplashAndLogin/NewSplashAndLogin.Android/App.cs
@@ -1,8 +1,23 @@
-﻿using Xamarin.Forms;
+﻿using NewSplashAndLogin.Views;
+using System;
+using Xamarin.Forms;
 
 namespace NewSplashAndLogin.Droid
 {
     internal class App : Application
     {
+        //public partial class App : Application
+       // {
+            public App()
+            {
+                InitializeComponent();
+                MainPage = new LoginPage();
+            }
+   // ...
+private void InitializeComponent()
+            {
+                throw new NotImplementedException();
+            }
+       // }
     }
 }
